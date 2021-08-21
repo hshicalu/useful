abdface7f86bd9d393299fc48c7967c8ee439cc1
hicalu <hshicalu@gmail.com>
2021-08-21 13:56:26 +0900

    algorithm

---
+++ algorithm/bs.cpp
@@ -0,0 +1,20 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool P(int x) {
+
+    }
+
+int binary_search() {
+    int left, right;
+
+    while (right - left > 1) {
+        int mid = left + (right - left) / 2;
+        int (P(mid)) right = mid;
+        else left = mid;
+
+        }
+    return right;
+    }
